740d920211b2193b63ff38cfb5691d9b141883f9
Sowmya <727823tuit225@skct.edu.in>
2024-12-01 20:04:01 +0530

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Task1{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+
+        int ans=in.nextInt();
+        int guess=in.nextInt();
+
+        final int MAX=100;
+
+        Random rand=new Random();
+
+    }
+}
